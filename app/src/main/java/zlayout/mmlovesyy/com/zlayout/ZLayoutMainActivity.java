d2a113bfda738503cfa754bb4fc40877c8324594
cmm <cmm@cmmdeMacBook-Pro.local>
2016-04-25 22:41:09 +0800

    completed samples

--- app/src/main/java/zlayout/mmlovesyy/com/zlayout/ZLayoutMainActivity.java
+++ app/src/main/java/zlayout/mmlovesyy/com/zlayout/ZLayoutMainActivity.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.Toast;
 
 public class ZLayoutMainActivity extends AppCompatActivity {
 
@@ -13,13 +12,11 @@ public class ZLayoutMainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_zlayout_main);
 
-        Toast.makeText(getApplicationContext(), "click to add images...", Toast.LENGTH_LONG).show();
-
         final ZLayout zLayout = (ZLayout) findViewById(R.id.z);
-        findViewById(R.id.screen).setOnClickListener(new View.OnClickListener() {
+
+        findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 ImageView img = new ImageView(getApplicationContext());
                 img.setImageDrawable(getResources().getDrawable(R.mipmap.ic_launcher));
                 zLayout.addView(img);
