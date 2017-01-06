6c2aa9f7669cb30b0200723efb0ca7aad1c93159
cmm <mengmeng.chen@dianping.com>
2017-01-06 13:49:20 +0800

    修改demo

--- app/src/main/java/com/mmlovesyy/zlayout/TagDemoActivity.java
+++ app/src/main/java/com/mmlovesyy/zlayout/TagDemoActivity.java
@@ -18,6 +18,7 @@ public class TagDemoActivity extends AppCompatActivity {
         final ZLayout zLayout = (ZLayout) findViewById(R.id.tag_demo_z);
 
         String[] tagText = {
+                "少年，干了这碗鸡汤：",
                 "凡事",
                 "坚持到最后",
                 "结果总会是好的",
@@ -31,29 +32,48 @@ public class TagDemoActivity extends AppCompatActivity {
 
     private void addTag(ZLayout zLayout, String[] tagText) {
 
-        for (String t : tagText) {
+        for (int i = 0; i < tagText.length; i++) {
 
-            final TagTextView tagview = new TagTextView(getApplicationContext());
-            tagview.setSingleLine(true);
-            tagview.setEllipsize(TextUtils.TruncateAt.END);
-            tagview.setText(t);
-            tagview.setTextSize(TypedValue.COMPLEX_UNIT_SP, 12);
-            tagview.setBackgroundDrawable(getResources().getDrawable(R.drawable.background_tag));
-            tagview.setTextColor(getResources().getColorStateList(R.color.tag_color));
-            tagview.setPadding(25, 15, 25, 15);
-            tagview.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    tagview.setChecked(!tagview.isChecked());
-                }
-            });
+            String t = tagText[i];
+            final TagTextView tagView = new TagTextView(getApplicationContext());
 
-            ViewGroup.MarginLayoutParams lp = new ZLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            // layout parameters
+            ViewGroup.MarginLayoutParams lp;
+
+            if (i == 0) {
+
+                // title
+                tagView.setTextColor(0xff333333);
+                tagView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 14);
+
+                lp = new ZLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+
+            } else {
+
+                tagView.setEllipsize(TextUtils.TruncateAt.END);
+                tagView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 12);
+                tagView.setBackgroundDrawable(getResources().getDrawable(R.drawable.background_tag));
+                tagView.setTextColor(getResources().getColorStateList(R.color.tag_color));
+                tagView.setPadding(25, 15, 25, 15);
+                tagView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        tagView.setChecked(!tagView.isChecked());
+                    }
+                });
+
+                lp = new ZLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+
+            }
+
+            tagView.setText(t);
+            tagView.setSingleLine(true);
+
             lp.leftMargin = 20;
             lp.topMargin = 10;
             lp.rightMargin = 0;
             lp.bottomMargin = 0;
-            zLayout.addView(tagview, lp);
+            zLayout.addView(tagView, lp);
         }
     }
 }
